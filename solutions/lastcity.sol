f02f4d971ae714986f7469dd8109f892ecbf4bb6
Joseph Rawson <joseph.rawson.works@littledebian.org>
2015-07-17 17:41:46 -0500

    git-annex automatic sync

---
+++ solutions/lastcity.sol
@@ -0,0 +1,1 @@
+../.git/annex/objects/MX/fX/SHA256E-s979--24cedceefac3bccc860fb2449108d238f298aa694706a3ea0da2aad7fa93c932.sol/SHA256E-s979--24cedceefac3bccc860fb2449108d238f298aa694706a3ea0da2aad7fa93c932.sol
