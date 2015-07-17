c1c77cb65b093f06eed21b833ceb5fff7f370da7
Joseph Rawson <joseph.rawson.works@littledebian.org>
2015-07-17 15:37:41 -0500

    git-annex automatic sync

---
+++ infocom/compilers/inform6/library/contributions/random.h
@@ -0,0 +1,1 @@
+../../../../../.git/annex/objects/2Z/z3/SHA256E-s12333--e4f42a9178be27a4f31cff305d5f236cc887faaba6973b13a13d2f100556f6b5.h/SHA256E-s12333--e4f42a9178be27a4f31cff305d5f236cc887faaba6973b13a13d2f100556f6b5.h
---
+++ solutions/Mindwheel.sol
@@ -0,0 +1,1 @@
+../.git/annex/objects/ZZ/1k/SHA256E-s13116--159d8bcbcfabc5760b17ea628302147f66e8e6aeb2cabdb88e65620cd09499d6.sol/SHA256E-s13116--159d8bcbcfabc5760b17ea628302147f66e8e6aeb2cabdb88e65620cd09499d6.sol
