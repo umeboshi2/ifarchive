02c504f59e8f0afd6186b8350c67a8c085e8f249
Joseph Rawson <joseph.rawson.works@littledebian.org>
2015-07-17 17:44:07 -0500

    git-annex automatic sync

---
+++ solutions/aztec.sol
@@ -0,0 +1,1 @@
+../.git/annex/objects/K9/GX/SHA256E-s1024--51dca691020ddaa99d2af599449bde3535b18cfbbddcf608bd4d712a6bd48162.sol/SHA256E-s1024--51dca691020ddaa99d2af599449bde3535b18cfbbddcf608bd4d712a6bd48162.sol
