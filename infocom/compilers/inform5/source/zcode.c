2166fb1439d5771ddf6be379d2ae99a7ce83ef58
Joseph Rawson <joseph.rawson.works@littledebian.org>
2015-07-17 15:19:30 -0500

    git-annex automatic sync

---
+++ infocom/compilers/inform5/source/zcode.c
@@ -0,0 +1,1 @@
+../../../../.git/annex/objects/q7/6j/SHA256E-s36474--28ed68ec3f3d75bdbfb3ef42d03d2013859b46d1d5089ed0054a6e9aedb004e2.c/SHA256E-s36474--28ed68ec3f3d75bdbfb3ef42d03d2013859b46d1d5089ed0054a6e9aedb004e2.c
