e0bbc73c648d5aee11726b6729dbdd92feb9bba3
Joseph Rawson <joseph.rawson.works@littledebian.org>
2015-07-16 22:03:17 -0500

    git-annex automatic sync

---
+++ games/competition2003/alan/apus/apus.sol
@@ -0,0 +1,1 @@
+../../../../.git/annex/objects/km/9M/SHA256E-s760--68e819eca1ea697bdd4afc2aa862971e526b9cd96464b875aa35a6893d0a4ae7.sol/SHA256E-s760--68e819eca1ea697bdd4afc2aa862971e526b9cd96464b875aa35a6893d0a4ae7.sol
