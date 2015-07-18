39185a5186c81d20ac40a77f7afe660dc3a7c43c
Joseph Rawson <joseph.rawson.works@littledebian.org>
2015-07-17 20:05:16 -0500

    git-annex automatic sync

---
+++ infocom/compilers/inform5/library/contributions/adhints/AdHints.h
@@ -0,0 +1,1 @@
+../../../../../../.git/annex/objects/pW/wk/SHA256E-s15093--3fdd1d1c7cc2eafcf9414795a02b7a29fc10f1805c227394682b8129b09b7ef3.h/SHA256E-s15093--3fdd1d1c7cc2eafcf9414795a02b7a29fc10f1805c227394682b8129b09b7ef3.h
