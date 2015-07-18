25f856c57c15c50c1916c16a2256c140d7c6ff09
Joseph Rawson <joseph.rawson.works@littledebian.org>
2015-07-17 21:01:44 -0500

    git-annex automatic sync

---
+++ programming/hugo/library/contributions/Smrtdesk.h
@@ -0,0 +1,1 @@
+../../../../.git/annex/objects/k9/gZ/SHA256E-s7729--9169b9df20df48f2b04cfbd5c26e855cb55e28fd5aeca39366dc6e76f0e8df29.h/SHA256E-s7729--9169b9df20df48f2b04cfbd5c26e855cb55e28fd5aeca39366dc6e76f0e8df29.h
