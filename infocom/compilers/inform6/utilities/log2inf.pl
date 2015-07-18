b75fd4189e43b811f761ca84df3ced37449a6793
Joseph Rawson <joseph.rawson.works@littledebian.org>
2015-07-17 19:48:46 -0500

    git-annex automatic sync

---
+++ infocom/compilers/inform6/utilities/log2inf.pl
@@ -0,0 +1,1 @@
+../../../../.git/annex/objects/g2/Jm/SHA256E-s15792--a234c6e6f55d3a944ed882e3a1c94b24eb23577deca6d9c80cac183024a46e70.pl/SHA256E-s15792--a234c6e6f55d3a944ed882e3a1c94b24eb23577deca6d9c80cac183024a46e70.pl
